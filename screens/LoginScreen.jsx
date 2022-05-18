77c645d49d4eb50a379e23025f9633eee59c5703
Pranav Murali <pranavmurali2001@gmail.com>
2022-05-19 00:02:14 +0530

    :maple_leaf: Migrate from twrnc to class styling

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,6 +1,5 @@
 import React ,{useEffect, useState} from 'react'
 import { Image, Text, View,SafeAreaView,TextInput} from 'react-native';
-import tw from "twrnc";
 import { useNavigation} from '@react-navigation/core';
 import { useStateValue } from "../StateProvider";
 import DeviceInfo from 'react-native-device-info';
@@ -43,16 +42,16 @@ const LoginScreen = () => {
   }, [phoneNumber]);
 
   return (
-    <SafeAreaView style={tw`bg-white h-full flex-1`}>
+    <SafeAreaView style={{flex:1,backgroundColor:"white",height:"100%"}}>
         <View >
-            <Image style={tw`ml-20 mt-8 w-64 h-32`} source={require("../Unipe-Thumbnail.png")}/>
+            <Image style={{marginLeft:70,marginTop:30,width:250,height:87}} source={require("../Unipe-Thumbnail.png")}/>
             <Text style={{fontSize:16,color:"#230C45",marginLeft:75,marginTop:40,fontFamily:"Roboto"}}>Enter Mobile Number for Verification</Text>
             <Text style={{fontSize:14,fontFamily:"Noto Sans",marginTop:60,marginLeft:40,color:"#020614"}}>Mobile Number</Text>
             {console.log(DeviceInfo.getPhoneNumber())}
-            <TextInput style={tw`ml-10 w-80 h-12 border-b-2`} autoFocus={true} value={phoneNumber} onChangeText={setPhoneNumber} autoCompleteType="tel" keyboardType="phone-pad" textContentType="telephoneNumber"/>
-            {next ? <Button uppercase={false} title="Continue" type="solid" style={tw`p-2 ml-10 mt-18 w-80 h-14 text-xl text-center content-center`} color="#4E46F1" onPress={() => {signIn(phoneNumber) }}/>: <Button uppercase={false} title="Continue" type="solid" style={tw` p-2 ml-10 mt-18 w-80 h-14 text-xl text-center`} disabled/>}
-            {/* <Button uppercase={false} title="ESCAPE" type="solid" style={tw`p-2 ml-10 mt-5 w-80 h-14 text-xl`} color="#4E46F1" onPress={() => {navigation.navigate("PersonlInfoForm") }}/>
-            <Button uppercase={false} title="ESCAPE home" type="solid" style={tw`ml-10 mt-5 w-80 h-14 text-xl`} onPress={() => {navigation.navigate("Home") }}/>   */}
+            <TextInput style={{marginLeft:40,width:320,height:50,borderBottomWidth:1}} value={phoneNumber} onChangeText={setPhoneNumber} autoCompleteType="tel" keyboardType="phone-pad" textContentType="telephoneNumber"/>
+            {next ? <Button uppercase={false} title="Continue" type="solid" style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}} color="#4E46F1" onPress={() => {signIn(phoneNumber) }}/>: <Button uppercase={false} title="Continue" type="solid" style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}} disabled/>}
+            <Button uppercase={false} title="ESCAPE" type="solid" style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}} color="#4E46F1" onPress={() => {navigation.navigate("PersonlInfoForm") }}/>
+            <Button uppercase={false} title="ESCAPE home" type="solid" style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}} onPress={() => {navigation.navigate("Home") }}/>  
         </View>
     </SafeAreaView>
   )
--- screens/OTPScreen.jsx
+++ screens/OTPScreen.jsx
@@ -1,6 +1,5 @@
 import React ,{useEffect, useState} from 'react'
 import { Image, Text, View,SafeAreaView,TextInput, KeyboardAvoidingView} from 'react-native';
-import tw from 'twrnc';
 import { Button,Icon,Grid} from "@react-native-material/core";
 import { useNavigation} from '@react-navigation/core';
 import { useStateValue } from "../StateProvider";
@@ -14,7 +13,7 @@ const OTPScreen = () => {
   async function confirmVerificationCode(code) {
     console.log(conf,phn);
     try {
-      // navigation.navigate('PersonlInfoForm');
+      navigation.navigate('PersonlInfoForm');
     } catch (error) {
       alert('Invalid code');
     }
@@ -47,14 +46,14 @@ const OTPScreen = () => {
 
   return (
     <SafeAreaView>
-      <View style={tw`bg-white h-full`}>
-          <Image style={tw`ml-20 mt-8 w-64 h-32`} source={require("../Unipe-Thumbnail.png")}/>
+      <View style={{backgroundColor:"white",height:"100%"}}>
+      <Image style={{marginLeft:70,marginTop:30,width:250,height:87}} source={require("../Unipe-Thumbnail.png")}/>
           <Text style={{color:"#230C45",marginLeft:65,marginTop:30,fontSize:16,fontFamily:"Roboto"}} >Please wait, we will auto verify the OTP {'\n'}                 sent to {phn}
           <Icon name="edit" size={12} color="#4E46F1" onPress={() =>navigation.navigate('Login')}/>
           </Text>
-          <TextInput style={tw`ml-28 mt-20 w-44 h-12 border-b-2`} autoFocus={true} letterSpacing={23} maxLength={6} numeric value={otp} onChangeText={setOtp} keyboardType="numeric"/>
+          <TextInput style={{marginLeft:105,marginTop:40,width:180,height:50,borderBottomWidth:1}} letterSpacing={23} maxLength={6} numeric value={otp} onChangeText={setOtp} keyboardType="numeric"/>
           <Text style={{color:"#4E46F1",marginLeft:155,marginTop:30,fontSize:16,fontFamily:"Roboto"}} onPress={()=>{ResendOtp()}}>Resend OTP</Text>
-          {next ? <Button uppercase={false} title="Verify" type="solid"  color="#4E46F1" style={tw` p-2 ml-10 mt-5 w-80 h-14 text-xl rounded`} onPress={() => {confirmVerificationCode(otp)}}><Text>Verify</Text></Button> : <Button title="Verify" uppercase={false} type="solid" style={tw`p-2 ml-10 mt-5 w-80 h-14 text-xl rounded`} disabled/>}
+          {next ? <Button uppercase={false} title="Verify" type="solid"  color="#4E46F1" sty style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}}le={{padding:4,marginTop:30,marginLeft:100}} onPress={() => {confirmVerificationCode(otp)}}><Text>Verify</Text></Button> : <Button title="Verify" uppercase={false} type="solid"  style={{padding:10,marginLeft:40,marginTop:50,width:320,height:60,fontSize:20}} disabled/>}
       </View>
     </SafeAreaView>
   )
