21a6705945877865a77bd60b589b45dc7c594881
Pranav Murali <pranavmurali2001@gmail.com>
2022-05-20 10:45:17 +0530

    Removed all files that are in the .gitignore

--- screens/PersonalDetailsForm.jsx
+++ screens/PersonalDetailsForm.jsx
@@ -5,47 +5,24 @@ import { useStateValue } from "../StateProvider";
 import { useNavigation} from '@react-navigation/core';
 import { AppBar,IconButton,Icon, Button} from "@react-native-material/core";
 import {ProgressBar} from '@react-native-community/progress-bar-android';
-import { form,progressBar} from './styles';
+import { form,progressBar, styles} from './styles';
 
 export default PesonalDetailsForm = () => {
   const navigation = useNavigation();
   const [{user},dispatch] = useStateValue();
-  const [male,setMale] = useState(false);
-  const [female,setFemale] = useState(false);
-  const [married,setMarried] = useState(false);
-  const [unmarried,setUnmarried] =useState(false);
-  const [users,setUsers]=useState("");
-  const [selectedValue,setSelectedValue] = useState("");
-  useEffect(() => {
-    if(male){
-      setFemale(false);
-    }
-  }, [male]);
-
-  useEffect(() => {
-    if(female){
-      setMale(false);
-    }
-  }, [female]);
-
-
-  useEffect(() => {
-    if(married){
-      setUnmarried(false);
-    }
-  }, [married]);
-
-  useEffect(() => {
-    if(unmarried){
-      setMarried(false);
-    }
-  }, [unmarried]);
+  const [gender,setGender] = useState('');
+  const [maritalStatus,setMaritalStatus] = useState('');
+  const [fullName,setFullName]=useState("");
+  const [educationalQualification,setEducationallQualification] = useState("");
+  const Educational_Qualification=[ "10th Pass", "12th Pass", "Graduate", "Post Graduate","None of the Above"];
+  const genders=["Male","Female"];
+  const maritalStatuslist=["Unmarried","Married"];
 
   useEffect(() => {
     dispatch({
-      type: "SET_USER",
-      payload: users,
-    })}, [users]);
+      type: "SET_FULL_NAME",
+      payload: fullName,
+    })}, [fullName]);
   
 
   return (
@@ -69,31 +46,35 @@ export default PesonalDetailsForm = () => {
     </View>
     
     <Text style={form.formHeader}>Welcome Let's Start your onboarding process</Text>
-    {users ? <Text style={form.formLabel}>Full Name</Text>:null}
-    <TextInput style={form.formTextInput} value={users} onChangeText={setUsers}  placeholder="Full Name"/>
+    {fullName ? <Text style={form.formLabel}>Full Name</Text>:null}
+    <TextInput style={form.formTextInput} value={fullName} onChangeText={setFullName}  placeholder="Full Name"/>
     <Text style={form.formLabel}>Gender</Text>
-    <View style={{flexDirection:'row'}}>
-    {male ? <Button uppercase={false} style={form.chosenButton} title="Male" type="solid" color="#4E46F1" onPress={() => {setMale(false)}}>Male</Button> : <Button uppercase={false} style={form.choiceButton} type="solid" title="Male" onPress={() => {setMale(true)}}>Male</Button>}
-    {female ? <Button uppercase={false} style={form.chosenButton} title="Female" type="solid" color="#4E46F1" onPress={() => {setFemale(false)}}>Female</Button> : <Button uppercase={false} style={form.choiceButton} type="solid" title="Female" onPress={() => {setFemale(true)}}>Female</Button>}
+    <View style={styles.flexrow}>
+    {genders.map((item,index)=>{
+      return(
+        <Button key={index} uppercase={false} style={gender==item ? form.chosenButton :form.choiceButton} title={item} type="solid" color="#4E46F1" onPress={()=>setGender(item)}/>
+      )
+    })}
     </View>
     <Text style={form.formLabel}>Select Education</Text>
       <Picker
-        selectedValue={selectedValue}
+        selectedValue={educationalQualification}
         style={form.picker}
-        onValueChange={(itemValue) => setSelectedValue(itemValue)}
+        onValueChange={(itemValue) => setEducationallQualification(itemValue)}
+        prompt="Educational Qualification"
       >
-        <Picker.Item label="Educational Qualification"/>
-        <Picker.Item label="10th Pass" value="10th pass"/>
-        <Picker.Item label="12th Pass" value="12th pass"/>
-        <Picker.Item label="Graduate"  value="Graduate"/>
-        <Picker.Item label="Diploma" value="Diploma"/>
-        <Picker.Item label="Post-Graduate" value="Post-Graduate"/>
-        <Picker.Item label="None of the Above" value="None of the Above"/>
+        {Educational_Qualification.map((item,index) => {
+          return(<Picker.Item label={item} value={item}/>)
+        })}
+        
       </Picker>
     <Text style={form.formLabel} >Marital Status</Text>
-    <View style={{flexDirection:'row'}}>
-    {married ? <Button  style={form.chosenButton}  uppercase={false} title="Married" type="solid" color="#4E46F1" onPress={() => {setMarried(false)}}>Married</Button> : <Button uppercase={false} style={form.choiceButton}  type="solid" title="Married" onPress={() => {setMarried(true)}}>Married</Button>}
-    {unmarried ? <Button style={form.chosenButton}  uppercase={false} title="Unmarried" type="solid" color="#4E46F1" onPress={() => {setUnmarried(false)}}>Unmarried</Button> : <Button uppercase={false} style={form.choiceButton}  type="solid" title="Unmarried" onPress={() => {setUnmarried(true)}}>Unmarried</Button>}
+    <View style={styles.flexrow}>
+    {maritalStatuslist.map((item,index)=>{
+      return(
+        <Button key={index} uppercase={false} style={maritalStatus==item ? form.chosenButton : form.choiceButton} title={item} type="solid" color="#4E46F1" onPress={()=>setMaritalStatus(item)}/>
+      )
+    })}
     </View>
     <Button title="Continue" type="solid"  uppercase={false} style={form.nextButton} color="#4E46F1" onPress={()=>{navigation.navigate("AadharForm")}}/>
     </SafeAreaView>
