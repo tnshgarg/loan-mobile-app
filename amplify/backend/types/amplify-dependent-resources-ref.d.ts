40102b785e55d172c5a8f85d03b9f4153d77cfcc
Pranav Murali <pranavmurali2001@gmail.com>
2022-06-02 01:42:33 +0530

    :cloud: Amplify setup auth base

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,43 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "amplifyuserGroupRole": "string"
+        },
+        "employeeapp9beaf1ce9beaf1ce": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "employeeapp9beaf1ce9beaf1ceCreateAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "employeeapp9beaf1ce9beaf1ceDefineAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "employeeapp9beaf1ce9beaf1cePreSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "employeeapp9beaf1ce9beaf1ceVerifyAuthChallengeResponse": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    }
+}
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -6,20 +6,50 @@ import DeviceInfo from 'react-native-device-info';
 import { Button } from "@react-native-material/core";
 import {styles} from "./styles";
 
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
+
 export default LoginScreen = () => {
   const navigation = useNavigation();
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [user, setUser] = useState(null);
   const [next, setNext] = useState(false);
   const [{conf},dispatch] = useStateValue();
+  const [session,setSession] = useState(null); 
   const [confirm, setConfirm] = useState(null);
+  const password = Math.random().toString(10) + 'Abc#';
 
-  async function signIn(phoneNumber) {
-    try {
-      navigation.navigate('Otp');
-    } catch (error) {
-      alert(error);
-    }
-  }
+  const signIn = () => {
+    Auth.signIn(phoneNumber)
+      .then((result) => {
+        setSession(result);
+        console.log(result);
+        navigation.navigate("Otp");
+      })
+      .catch((e) => {
+        if (e.code === 'UserNotFoundException') {
+          signUp(); // Note that this is a new function to be created later
+          console.log('User not found');
+        } else if (e.code === 'UsernameExistsException') {
+          signIn();
+        } else {
+          console.log(e.code);
+          console.error(e);
+        }
+      });
+  };
+  const signUp = async () => {
+    const result = await Auth.signUp({
+      username: phoneNumber,
+      password,
+      attributes: {
+        phone_number: phoneNumber,
+      },
+    }).then(() => signIn()); // After signUp, we are going to signIn() 
+    return result;
+  };
 
   useEffect(() => {
     dispatch({
@@ -34,7 +64,7 @@ export default LoginScreen = () => {
     })}, [phoneNumber]);
 
   useEffect(() => {
-    if(phoneNumber.length === 10){
+    if(phoneNumber.length === 13){
       setNext(true);
     }
     else{
@@ -50,7 +80,7 @@ export default LoginScreen = () => {
             <Text style={styles.fieldLabel}>Mobile Number</Text>
             {console.log(DeviceInfo.getPhoneNumber())}
             <TextInput style={styles.textInput} value={phoneNumber} onChangeText={setPhoneNumber} autoCompleteType="tel" keyboardType="phone-pad" textContentType="telephoneNumber"/>
-            {next ? <Button uppercase={false} title="Continue" type="solid" style={styles.ContinueButton} color="#4E46F1" onPress={() => {signIn(phoneNumber) }}/>: <Button uppercase={false} title="Continue" type="solid" style={styles.ContinueButton} disabled/>}
+            {next ? <Button uppercase={false} title="Continue" type="solid" style={styles.ContinueButton} color="#4E46F1" onPress={() => {signIn()}}/>: <Button uppercase={false} title="Continue" type="solid" style={styles.ContinueButton} disabled/>}
         </View>
     </SafeAreaView>
   )
