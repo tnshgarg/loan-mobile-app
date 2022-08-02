bdbdfb8be9704f318b7455d0a258000d9567182b
Tanish garg <tanishg566@outlook.com>
2022-08-02 19:42:53 +0530

    added useselector on adhaar

--- screens/07_drawer/Aadhaar.jsx
+++ screens/07_drawer/Aadhaar.jsx
@@ -2,19 +2,27 @@ import { View, Text, Alert } from "react-native";
 import React from "react";
 import DetailItem from "./DetailItem";
 import PrimaryButton from "../../components/PrimaryButton";
+import { useSelector } from "react-redux";
 
 const Aadhaar = () => {
+  const aadhaarNumber = useSelector((state) => state.aadhaar.number);
+  const verifyStatus = useSelector((state) => state.aadhaar.verifyStatus);
+
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <DetailItem label="Full Name" title="Tanish Garg" divider />
-      <DetailItem label="Aadhaar Number" title="7895 5458 5448" divider />
+      <DetailItem
+        label="Aadhaar Number"
+        title={JSON.stringify("aadhaarNumber")}
+        divider
+      />
       <DetailItem label="DOB" title="07/07/2000" divider />
       <DetailItem
         label="Address"
         title="Vasant Vihar, Kaithal, Haryana, 136027"
         divider
       />
-      <DetailItem label="Verify Status" title="Pending" />
+      <DetailItem label="Verify Status" title={"verifyStatus"} />
       <View style={{ flex: 1, justifyContent: "flex-end", paddingBottom: 20 }}>
         <PrimaryButton
           style={{ marginTop: 20 }}
