3052c248cf6a459f0f2be5e5b95dd76f5382a2e9
Pranav Murali <pranavmurali2001@gmail.com>
2022-08-08 12:53:24 +0530

    rm aadhaarCameraSetup , BackendPush

--- components/RNIPPhotoCapture.jsx
+++ components/RNIPPhotoCapture.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Icon, IconButton } from "@react-native-material/core";
 import * as ImagePicker from "react-native-image-picker";
 import { useDispatch } from "react-redux";
-import { addAadhaarImage } from "../store/slices/aadhaarSlice";
 import { addSelfie } from "../store/slices/profileSlice";
 import { selfie } from "../styles";
+
 export default function RNIPPhotoCapture(props) {
   const dispatch = useDispatch();
   const openCamera = () => {
@@ -20,16 +20,7 @@ export default function RNIPPhotoCapture(props) {
       } else if (response.error) {
         console.log("ImagePicker Error: ", response.error);
       } else {
-        if (props.type.match(/^AADHAAR/)) {
-          dispatch(
-            addAadhaarImage({
-              data: response?.assets && response.assets[0].base64,
-              type: props.type,
-            })
-          );
-        } else if (props.type.match(/^SELFIE/)) {
-          dispatch(addSelfie(response?.assets && response.assets[0].base64));
-        }
+        dispatch(addSelfie(response?.assets && response.assets[0].base64));
       }
     });
   };
