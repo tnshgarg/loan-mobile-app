4b3d8d5697278dfca7e9343d9b9186e78e759be3
Pranav Murali <pranavmurali2001@gmail.com>
2022-08-29 11:15:17 +0530

    Update Mandate.jsx

--- screens/06_home/EWA/Mandate.jsx
+++ screens/06_home/EWA/Mandate.jsx
@@ -1,13 +1,38 @@
 import { AppBar, IconButton } from "@react-native-material/core";
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, Text, TextInput, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import PrimaryButton from "../../../components/PrimaryButton";
-import { styles } from "../../../styles";
+import { KeyboardAvoidingWrapper } from "../../../KeyboardAvoidingWrapper";
+import { form, bankform, styles } from "../../../styles";
+import Verify from "../../../apis/bank/Verify";
 
 const Mandate = () => {
   const navigation = useNavigation();
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [ifsc, setIfsc] = useState("");
+  const [ifscNext, setIfscNext] = useState(false);
+  const [accNumNext, setAccNumNext] = useState(false);
+
+  useEffect(() => {
+    var accountNumberReg = /^[0-9]{9,18}$/gm;
+    if (accountNumberReg.test(number)) {
+      setAccNumNext(true);
+    } else {
+      setAccNumNext(false);
+    }
+  }, [number]);
+
+  useEffect(() => {
+    var ifscReg = /^[A-Z]{4}0[A-Z0-9]{6}$/gm;
+    if (ifscReg.test(ifsc)) {
+      setIfscNext(true);
+    } else {
+      setIfscNext(false);
+    }
+  }, [ifsc]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -23,13 +48,50 @@ const Mandate = () => {
           />
         }
       />
-      <PrimaryButton
-        title="My Details are Correct"
-        uppercase={false}
-        onPress={() => {
-          navigation.navigate("EWALoanDetails");
-        }}
-      />
+      <KeyboardAvoidingWrapper>
+        <View>
+          <Text style={bankform.formtitle}>Account Holder Name</Text>
+          <TextInput
+            style={bankform.formInput}
+            autoCapitalize="words"
+            value={name}
+            onChangeText={setName}
+            required
+          />
+          <Text style={bankform.formtitle}>Bank Account Number</Text>
+          <TextInput
+            style={bankform.formInput}
+            autoCapitalize="words"
+            value={number}
+            onChangeText={setNumber}
+            required
+          />
+          {number && !accNumNext ? (
+            <Text style={bankform.formatmsg}>Incorrect Format</Text>
+          ) : null}
+
+          <Text style={bankform.formtitle}>IFSC</Text>
+          <TextInput
+            style={bankform.formInput}
+            autoCapitalize="characters"
+            value={ifsc}
+            onChangeText={setIfsc}
+            required
+          />
+          {ifsc && !ifscNext ? (
+            <Text style={bankform.formatmsg}>Incorrect Format</Text>
+          ) : null}
+
+          <PrimaryButton
+            title="My Details are Correct"
+            uppercase={false}
+            onPress={() => {
+              navigation.navigate("EWALoanDetails");
+            }}
+          />
+          <View style={bankform.padding}></View>
+        </View>
+      </KeyboardAvoidingWrapper>
     </SafeAreaView>
   );
 };
