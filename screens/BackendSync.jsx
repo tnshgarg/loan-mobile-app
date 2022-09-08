adeac0070bf42fce0379b6a732bc48b928fa1ef1
supatomic <kngoyal.cs@gmail.com>
2022-09-08 13:12:35 -0400

    fetch pan data on login

--- screens/BackendSync.jsx
+++ screens/BackendSync.jsx
@@ -4,6 +4,7 @@ import { Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getBackendData } from "../services/employees/employeeServices";
 import { resetAadhaar } from "../store/slices/aadhaarSlice";
+import { resetPan } from "../store/slices/panSlice";
 
 export default BackendSync = (props) => {
 
@@ -24,11 +25,28 @@ export default BackendSync = (props) => {
         console.log(response.data);
         if (response.data.status === 200) {
           dispatch(resetAadhaar(response.data.body));
-          console.log("fecthedAadhaar",response.data.body);
+          console.log("aadhaarBackendFetch response.data", response.data);
         }
       })
       .catch((error) => {
         console.log("aadhaarBackendFetch error: ", error);
+      });
+    }
+  }, [id]);
+
+  useEffect(() => {
+    console.log("BackendSync id: ", id);
+    if (id) {
+      getBackendData({ params: {id: id}, xpath: "pan" })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status === 200) {
+          dispatch(resetPan(response.data.body));
+          console.log("panBackendFetch response.data", response.data);
+        }
+      })
+      .catch((error) => {
+        console.log("panBackendFetch error: ", error);
       });
     }
   }, [id]);
