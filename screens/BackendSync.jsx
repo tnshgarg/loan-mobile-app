d09801ce51613e34c3692341291db02190a1d918
Pranav Murali <pranavmurali2001@gmail.com>
2022-09-09 01:20:02 +0530

    Update BackendSync.jsx

--- screens/BackendSync.jsx
+++ screens/BackendSync.jsx
@@ -4,9 +4,10 @@ import { Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getBackendData } from "../services/employees/employeeServices";
 import { resetAadhaar } from "../store/slices/aadhaarSlice";
+import { resetBank } from "../store/slices/bankSlice";
+import { resetProfile } from "../store/slices/profileSlice";
 
 export default BackendSync = (props) => {
-
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
@@ -19,21 +20,58 @@ export default BackendSync = (props) => {
   useEffect(() => {
     console.log("BackendSync id: ", id);
     if (id) {
-      getBackendData({ params: {id: id}, xpath: "aadhaar" })
-      .then((response) => {
-        console.log(response.data);
-        if (response.data.status === 200) {
-          dispatch(resetAadhaar(response.data.body));
-          console.log("fecthedAadhaar",response.data.body);
-        }
-      })
-      .catch((error) => {
-        console.log("aadhaarBackendFetch error: ", error);
-      });
+      getBackendData({ params: { id: id }, xpath: "aadhaar" })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.status === 200) {
+            dispatch(resetAadhaar(response.data.body));
+            console.log("fecthedAadhaar", response.data.body);
+          }
+        })
+        .catch((error) => {
+          console.log("aadhaarBackendFetch error: ", error);
+        });
     }
   }, [id]);
+
+  useEffect(() => {
+    console.log("BackendSync id: ", id);
+    if (id) {
+      getBackendData({ params: { id: id }, xpath: "bank" })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.status === 200) {
+            dispatch(resetBank(response.data.body));
+            console.log("fetchedBank", response.data.body);
+          }
+        })
+        .catch((error) => {
+          console.log("bankBackendFetch error: ", error);
+        });
+    }
+  }, [id]);
+
+  useEffect(() => {
+    console.log("BackendSync id: ", id);
+    if (id) {
+      getBackendData({ params: { id: id }, xpath: "profile" })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.status === 200) {
+            dispatch(resetProfile(response.data.body));
+            console.log("fetchedProfile", response.data.body);
+          }
+        })
+        .catch((error) => {
+          console.log("profileBackendFetch error: ", error);
+        });
+    }
+  }, [id]);
+
   
   return (
-      <Image source={require("../android/app/src/main/res/drawable/launch_screen.png")} />
+    <Image
+      source={require("../android/app/src/main/res/drawable/launch_screen.png")}
+    />
   );
 };
