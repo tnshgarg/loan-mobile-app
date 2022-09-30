40a2e5760221d603c312f46f843ca79db07acaa4
Pranav Murali <pranavmurali2001@gmail.com>
2022-09-30 10:48:33 +0530

    Home Offer Card + notation changes

--- screens/06_home/HomeView.jsx
+++ screens/06_home/HomeView.jsx
@@ -1,26 +1,18 @@
-import { Icon } from "@react-native-material/core";
-import React from "react";
-import { SafeAreaView, ScrollView, TextInput, View } from "react-native";
-import DocumentTile from "../../components/DocumentTile";
-import { docSearch, styles } from "../../styles";
+import { SafeAreaView, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { styles } from "../../styles";
+import KycCheckCard from "../../components/KycCheckCard";
+import HomeOfferCard from "../../components/HomeOfferCard";
 
-export default HomeView = () => {
-  const documents = [
-    { title: "Document 1", date: "2020-01-01" },
-    { title: "Document 2", date: "2020-01-01" },
-    { title: "Document 3", date: "2020-01-01" },
-  ];
+const HomeView = () => {
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <View style={docSearch.searchBar}>
-          <Icon style={docSearch.searchIcon} name="search" size={30} />
-          <TextInput style={docSearch.searchInput} placeholder="Search" />
-        </View>
-        <ScrollView>
-          <DocumentTile documents={documents} />
-        </ScrollView>
+        <KycCheckCard />
+        <HomeOfferCard />
       </SafeAreaView>
     </>
   );
 };
+
+export default HomeView;
