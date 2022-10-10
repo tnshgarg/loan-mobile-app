dd2feb6fb2e3863962a99a90132cf0a39eed9818
Pranav Murali <pranavmurali2001@gmail.com>
2022-10-10 16:29:29 +0530

    rm documents

--- screens/06_home/DocumentsView.jsx
+++ screens/06_home/DocumentsView.jsx
@@ -1,27 +1,20 @@
-import { Icon } from "@react-native-material/core";
 import React from "react";
-import { SafeAreaView, ScrollView, TextInput, View } from "react-native";
-import DocumentTile from "../../components/DocumentTile";
-import { docSearch, styles } from "../../styles";
+import { Image, View, SafeAreaView, Text } from "react-native";
+import { styles } from "../../styles";
 
 const DocumentsView = () => {
-  const documents = [
-    { title: "Document 1", date: "2020-01-01" },
-    { title: "Document 2", date: "2020-01-01" },
-    { title: "Document 3", date: "2020-01-01" },
-  ];
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <View style={docSearch.searchBar}>
-          <Icon style={docSearch.searchIcon} name="search" size={30} />
-          <TextInput style={docSearch.searchInput} placeholder="Search" />
-        </View>
-        <ScrollView>
-          <DocumentTile documents={documents} />
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignSelf: "center", marginTop: "20%" }}>
+        <Text style={{ fontSize: 20, alignSelf: "center" }}>
+          ✨Somethings Cooking✨
+        </Text>
+        <Image
+          style={{ width: 300, height: "70%" }}
+          source={require("../../assets/cooking.gif")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
