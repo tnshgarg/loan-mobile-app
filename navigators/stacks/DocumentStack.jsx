c15ea7d617f141aa0b6142569f46ff8278aaefa0
Tanish garg <tanishg566@outlook.com>
2022-11-03 09:42:31 +0530

    fixes

--- navigators/stacks/DocumentStack.jsx
+++ navigators/stacks/DocumentStack.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import OfflineAlert from "../components/OfflineAlert";
 import Documents from "../../screens/06_home/Documents/Documents";
 
 const DocumentStack = () => {
   const Stack = createNativeStackNavigator();
 
   return (
-    <OfflineAlert>
-      <Stack.Navigator initialRouteName={"Documents"}>
-        <Stack.Screen
-          name="Documents"
-          component={Documents}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </OfflineAlert>
+    <Stack.Navigator initialRouteName={"Documents"}>
+      <Stack.Screen
+        name="Documents"
+        component={Documents}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
   );
 };
 
