63c11df78db598166c6cc87a3711098c61e4416c
Tanish garg <tanishg566@outlook.com>
2023-01-03 19:23:33 +0530

    changes

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { navigationRef } from "./navigators/RootNavigation";
 import Analytics from "appcenter-analytics";
 import { STAGE } from "@env";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./queries/Repayment";
+import { queryClient } from "./queries/index";
 
 Crashes.setListener({
   shouldProcess: function (report) {
