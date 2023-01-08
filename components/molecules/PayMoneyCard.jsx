3257486bfdb20eb3a5e4396c4edadfefc456e55e
supatomic <kngoyal.cs@gmail.com>
2023-01-07 21:25:34 -0500

    remove getRepaymentData console.log

--- components/molecules/PayMoneyCard.jsx
+++ components/molecules/PayMoneyCard.jsx
@@ -55,7 +55,6 @@ const PayMoneyCard = () => {
   });
 
   useEffect(() => {
-    console.log("ewaRepaymentFetch API getRepaymentData.data: ", getRepaymentData.data);
     if (isFocused && !getRepaymentIsLoading && getRepaymentIsSuccess) {
       if (getRepaymentData.data.status === 200) {
         var repaymentAmount = Math.max(
