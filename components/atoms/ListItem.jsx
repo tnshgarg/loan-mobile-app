6535f02adbdaa4998499ff13bc4d641a5548e0cf
supatomic <kngoyal.cs@gmail.com>
2023-01-12 15:06:22 -0500

    add ripple effect to listitems

--- components/atoms/ListItem.jsx
+++ components/atoms/ListItem.jsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableNativeFeedback, View } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
 import { MaterialCommunityIcons } from "react-native-vector-icons";
 import { COLORS, FONTS } from "../../constants/Theme";
@@ -7,55 +7,59 @@ const ListItem = ({ item, disabled, showIcon, selected, titleStyle }) => {
   const { title, subtitle, iconName, onPress } = item;
 
   return (
-    <TouchableOpacity
+    <TouchableNativeFeedback
       accessibilityLabel="InfoCard"
-      style={[
-        styles.container,
-        {
-          backgroundColor: selected
-            ? COLORS.primary
-            : disabled
-            ? COLORS.lightgray_01
-            : COLORS.white,
-        },
-      ]}
-      activeOpacity={0.8}
       onPress={onPress}
       disabled={disabled}
     >
-      <MaterialCommunityIcons
-        name={iconName}
-        size={24}
-        color={selected ? COLORS.white : disabled ? COLORS.gray : COLORS.black}
-      />
-      <View style={styles.textContainer}>
-        <Text
-          style={[
-            styles.title,
-            { ...titleStyle },
-            {
-              color: selected
-                ? COLORS.white
-                : disabled
-                ? COLORS.gray
-                : COLORS.black,
-            },
-          ]}
-        >
-          {title}
-        </Text>
-        {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
-      </View>
-      {showIcon && (
+      <View
+        style={[
+          styles.container,
+          {
+            backgroundColor: selected
+              ? COLORS.primary
+              : disabled
+              ? COLORS.lightgray_01
+              : COLORS.white,
+          },
+        ]}
+      >
         <MaterialCommunityIcons
-          name="chevron-right"
+          name={iconName}
           size={24}
           color={
             selected ? COLORS.white : disabled ? COLORS.gray : COLORS.black
           }
         />
-      )}
-    </TouchableOpacity>
+        <View style={styles.textContainer}>
+          <Text
+            style={[
+              styles.title,
+              { ...titleStyle },
+              {
+                color: selected
+                  ? COLORS.white
+                  : disabled
+                  ? COLORS.gray
+                  : COLORS.black,
+              },
+            ]}
+          >
+            {title}
+          </Text>
+          {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
+        </View>
+        {showIcon && (
+          <MaterialCommunityIcons
+            name="chevron-right"
+            size={24}
+            color={
+              selected ? COLORS.white : disabled ? COLORS.gray : COLORS.black
+            }
+          />
+        )}
+      </View>
+    </TouchableNativeFeedback>
   );
 };
 
