5f0fb09790d0ecc2adc06b794dac30beb5d86525
Pranav Murali <pranavmurali2001@gmail.com>
2023-01-18 11:17:42 +0530

    keys updated

--- services/constants.js
+++ services/constants.js
@@ -4,8 +4,10 @@ import {
   KYC_MOCK_API_BASE_URL,
   RZP_PROD_AUTH,
   RZP_TEST_AUTH,
+  RZP_DEV_AUTH,
   RZP_PROD_KEY_ID,
   RZP_TEST_KEY_ID,
+  RZP_DEV_KEY_ID
 } from "@env";
 
 export const EMPLOYEE_API_URL = `https://api-${STAGE}.unipe.co/employee`;
@@ -18,7 +20,7 @@ export const KYC_AADHAAR_SUBMIT_OTP_API_URL = `${KYC_API_URL}/aadhaar-api/boson/
 export const KYC_PAN_VERIFY_API_URL = `${KYC_API_URL}/pan-api/fetch-detailed`;
 export const KYC_BANK_VERIFY_API_URL = `${KYC_API_URL}/bank-api/verify`;
 
-export const RZP_KEY_ID = STAGE !== "prod" ? RZP_TEST_KEY_ID : RZP_PROD_KEY_ID;
-export const RZP_AUTH = STAGE !== "prod" ? RZP_TEST_AUTH : RZP_PROD_AUTH;
+export const RZP_KEY_ID = STAGE !== "prod" ? (STAGE === "test" ? RZP_TEST_KEY_ID : RZP_DEV_KEY_ID)  : RZP_PROD_KEY_ID;
+export const RZP_AUTH = STAGE !== "prod" ? (STAGE === "test" ? RZP_TEST_AUTH : RZP_DEV_AUTH)  : RZP_PROD_AUTH;
 
 export const TIMEOUT = 5 * 60;
