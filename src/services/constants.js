55a3b61ea12abd4d58db820a83c0e62ffdba52ed
PranavMurali <pranavmurali2001@gmail.com>
2023-04-28 14:34:51 +0530

    constants + env changes

--- src/services/constants.js
+++ src/services/constants.js
@@ -7,7 +7,9 @@ import {
   RZP_DEV_AUTH,
   RZP_PROD_KEY_ID,
   RZP_QA_KEY_ID,
-  RZP_DEV_KEY_ID
+  RZP_DEV_KEY_ID,
+  OG_PROD_KEY,
+  OG_DEV_KEY,
 } from "@env";
 
 export const EMPLOYEE_API_URL = `https://api-${STAGE}.unipe.co/employee`;
@@ -22,5 +24,6 @@ export const KYC_BANK_VERIFY_API_URL = `${KYC_API_URL}/bank-api/verify`;
 
 export const RZP_KEY_ID = STAGE !== "prod" ? (STAGE === "qa" ? RZP_QA_KEY_ID : RZP_DEV_KEY_ID)  : RZP_PROD_KEY_ID;
 export const RZP_AUTH = STAGE !== "prod" ? (STAGE === "qa" ? RZP_QA_AUTH : RZP_DEV_AUTH)  : RZP_PROD_AUTH;
+export const OG_API_KEY = STAGE !== "prod" ? OG_DEV_KEY : OG_PROD_KEY
 
 export const TIMEOUT = 5 * 60;
