f0f9d07806ad4bcc4e5a9828dd88178179e9a93e
PranavMurali <pranavmurali2001@gmail.com>
2023-04-29 13:49:54 +0530

    gupshup configs

--- src/services/constants.js
+++ src/services/constants.js
@@ -20,6 +20,9 @@ export const KYC_AADHAAR_SUBMIT_OTP_API_URL = `${KYC_API_URL}/aadhaar-api/boson/
 export const KYC_PAN_VERIFY_API_URL = `${KYC_API_URL}/pan-api/fetch-detailed`;
 export const KYC_BANK_VERIFY_API_URL = `${KYC_API_URL}/bank-api/verify`;
 
+export const GUPSHUP_GENERATE_OTP_API_URL=`${EMPLOYEE_API_URL}/login/generate-otp`
+export const GUPSHUP_VERIFY_OTP_API_URL=`${EMPLOYEE_API_URL}/login/verify-otp`
+
 export const RZP_KEY_ID = STAGE !== "prod" ? (STAGE === "qa" ? RZP_QA_KEY_ID : RZP_DEV_KEY_ID)  : RZP_PROD_KEY_ID;
 export const RZP_AUTH = STAGE !== "prod" ? (STAGE === "qa" ? RZP_QA_AUTH : RZP_DEV_AUTH)  : RZP_PROD_AUTH;
 
