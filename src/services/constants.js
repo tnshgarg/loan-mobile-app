15c4ea425b13415fa7138a530648275dc1081f34
shubham <arawkar.shubham08@gmail.com>
2023-06-08 20:39:06 +0530

    fix: rzp key

--- src/services/constants.js
+++ src/services/constants.js
@@ -11,6 +11,6 @@ export const ANALYTICS_URL = "https://ate9ze8q87.execute-api.ap-south-1.amazonaw
 export const GUPSHUP_GENERATE_OTP_API_URL=`${EMPLOYEE_API_URL}/login/generate-otp`
 export const GUPSHUP_VERIFY_OTP_API_URL=`${EMPLOYEE_API_URL}/login/verify-otp`
 
-export const RZP_KEY_ID = STAGE !== "prod" ? (STAGE === "qa" ? RZP_QA_KEY_ID : RZP_DEV_KEY_ID)  : RZP_PROD_KEY_ID;
+export const RZP_KEY_ID = STAGE !== "prod" ? RZP_DEV_KEY_ID : RZP_PROD_KEY_ID;
 
 export const TIMEOUT = 5 * 60;
