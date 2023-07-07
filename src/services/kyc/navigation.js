3da837ef7335152c75fdc586dd01e52493207806
shubham <arawkar.shubham08@gmail.com>
2023-07-07 17:47:59 +0530

    mod: navigation fix

--- src/services/kyc/navigation.js
+++ src/services/kyc/navigation.js
@@ -57,10 +57,7 @@ export const kycNavigate = (kycData, navigation) => {
   }
 
   if (kycStage == KS.COMPLETE) {
-    navigate("CmsStack", {
-      screen: "CmsScreenOne",
-      params: { blogKey: "kyc_success" },
-    });
+    navigate("KycSuccess");
     return;
   }
   navigate("EWAStack", {
