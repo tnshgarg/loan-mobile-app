847c549b35096fb4a2c79783b31c930df55d6dcc
Pranav Murali <pranavmurali2001@gmail.com>
2023-08-28 10:14:28 +0530

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -8,7 +8,7 @@ const POLLING_DURATION_MAP = {
 export const CMS_POLLING_DURATION = POLLING_DURATION_MAP[STAGE] * 1000;
 export const KYC_POLLING_DURATION = 24 * 60 * 60 * 1000;
 export const EWA_POLLING_DURATION = 2 * 60 * 60 * 1000;
-export const EMPLOYEE_API_URL = `https://api-${STAGE}.unipe.money/employee`;
+export const EMPLOYEE_API_URL = `https://api-${STAGE}.unipe.money/mobile-iota`;
 console.log("api-url", EMPLOYEE_API_URL)
 export const ANALYTICS_URL = "https://ate9ze8q87.execute-api.ap-south-1.amazonaws.com/" + STAGE;
 export const RZP_KEY_ID = STAGE !== "prod" ? RZP_DEV_KEY_ID : RZP_PROD_KEY_ID;
